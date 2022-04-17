7d1b3223fa4e9bc9703de3824917ba1ec2554ba1
Guilherme Leao <leaob7@gmail.com>
2022-04-17 16:21:15 -0300

    feat: add initial router and redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   // Router Pages
   return (
-      <>
-        App
-      </>
+      <Switch>
+        <Route path="/" />
+        <Route path="/browse" />
+        <Route path="/browse:id" />
+        <Route path="/my-list" />
+      </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // Redux Provider
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={ store }>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
 
 reportWebVitals();
