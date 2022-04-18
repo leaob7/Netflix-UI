63533f9d44010b9d470cb6a993d7e2af3e55be26
Guilherme Leao <leaob7@gmail.com>
2022-04-18 12:00:26 -0300

    build: add dotenv + feat: add movies api requests

---
+++ src/utils/MoviesApiRequest.js
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+
+class MoviesRequests {
+  async getLatest() {
+    await fetch(`https://api.themoviedb.org/3/movie/latest?api_key=${process.env.API_KEY}&language=en-US`)
+    .then((response) => response.json());
+  }
+
+  async getPopular() {
+    await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`)
+    .then((response) => response.json());
+  }
+
+  async getNowPlaying() {
+    await fetch(`
+    https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en-US&page=1`)
+    .then((response) => response.json());
+  }
+
+  async getTopRated() {
+    await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`)
+    .then((response) => response.json());
+  }
+
+  async getUpcoming() {
+    await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_KEY}&language=en-US&page=1`)
+    .then((response) => response.json());
+  }
+}
+
+export default MoviesRequests;
