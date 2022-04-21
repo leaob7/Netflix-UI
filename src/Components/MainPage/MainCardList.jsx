9b43427efc9f2e670f826a5892fda0801671cd2f
Guilherme Leao <leaob7@gmail.com>
2022-04-21 15:03:24 -0300

    refactor: new logic to arrows list + styles: styles to arrow and lista

--- src/Components/MainPage/MainCardList.jsx
+++ src/Components/MainPage/MainCardList.jsx
@@ -1,20 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid, Typography  } from '@material-ui/core';
 import MidiaCard from './MidiaCard';
 import { NavigateBeforeSharp, NavigateNextSharp } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles({
+  listGrid: {
+    display: 'flex',
+    alignItems: "center",
+    justifyContent: 'center'
+  },
   listTitle: {
     color: 'white',
     marginTop: 10,
     marginLeft: 63,
     fontSize: '1.5vw'
+  },
+  listArrowRight: {
+    position: 'absolute',
+    width: 40,
+    height: '23vh',
+    right: 0,
+    color: 'white',
+    backgroundColor: 'rgb(0, 0, 0, 0.6)',
+  },
+  listArrowLeft: {
+    position: 'absolute',
+    width: 40,
+    height: '23vh',
+    left: 0,
+    color: 'white',
+    backgroundColor: 'rgb(0, 0, 0, 0.6)',
   }
 })
 
-function MainCardList({ movies, textField }) {
+function MainCardList({ movies: moviesDB, textField }) {
+  const movies = moviesDB.slice(0, 10);
+  const [scrollX, setScrollX] = useState(-400);
   const classes = useStyles();
+
+  const handleLeftArrow = () => {
+    let x = scrollX + Math.round(window.innerWidth / 2);
+    if(x > 0) {
+      x = 0;
+    }
+
+    setScrollX(x);
+    // movies list margin responsively on arrow click
+    // get the margin of the list and sum with the total window width
+    // => scrolls half of the users window
+  }
+
+  const handleRightArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    let moviesW = movies.length * 260;
+    // the length of the movies list
+    if((window.innerWidth - moviesW) > x) {
+      x = (window.innerWidth - moviesW);
+    }
+    setScrollX(x);
+  }
 
   return (
     <Grid
@@ -22,22 +67,25 @@ function MainCardList({ movies, textField }) {
     >
 
       <Typography
-        variant='h5'
-        color='primary'
         className={classes.listTitle}
       >
         {`${textField}`}
       </Typography>
 
       <Grid
-        style={{ display: 'flex', alignItems: "center" }}
+        style={{
+          display: 'flex',
+          alignItems: "center",
+          justifyContent: 'center',
+          marginLeft: scrollX,
+        }}
       >
 
-        <NavigateBeforeSharp color='primary'/>
+        <NavigateBeforeSharp className={classes.listArrowLeft} onClick={handleLeftArrow} />
 
-        <NavigateNextSharp color='primary' />
+        <NavigateNextSharp className={classes.listArrowRight} onClick={handleRightArrow} />
 
-        { movies.slice(0, 10).map((movie) => (
+        { movies.map((movie) => (
           
           <MidiaCard key={movie.id} movie={movie} />
           
