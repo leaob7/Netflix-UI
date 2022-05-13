f259454f96cbc8d09fb48239380e1da07bf585fd
Guilherme Leao <leaob7@gmail.com>
2022-05-13 19:28:08 -0300

    fix: fix deploy issues

--- src/API/MoviesApiRequest.js
+++ src/API/MoviesApiRequest.js
@@ -1,32 +1,32 @@
 class MoviesRequests {
   async getLatest() {
-    return fetch(`https://api.themoviedb.org/3/movie/latest?api_key=${process.env.REACT_APP_API_KEY}&language=pt-BR`)
+    return fetch(`https://api.themoviedb.org/3/movie/latest?api_key=e2a11eee9d189804182d267a87a4bea2&language=pt-BR`)
     .then((response) => response.json());
   }
 
   async getPopular(page) {
-    return fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=pt-BR&page=${page}`)
+    return fetch(`https://api.themoviedb.org/3/movie/popular?api_key=e2a11eee9d189804182d267a87a4bea2&language=pt-BR&page=${page}`)
     .then((response) => response.json());
   }
 
   async getNowPlaying() {
     return fetch(`
-    https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=pt-BR&page=1`)
+    https://api.themoviedb.org/3/movie/now_playing?api_key=e2a11eee9d189804182d267a87a4bea2&language=pt-BR&page=1`)
     .then((response) => response.json());
   }
 
   async getTopRated(page) {
-    return fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=pt-BR&page=${page}`)
+    return fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=e2a11eee9d189804182d267a87a4bea2&language=pt-BR&page=${page}`)
     .then((response) => response.json());
   }
 
   async getUpcoming() {
-    return fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_API_KEY}&language=pt-BR&page=1`)
+    return fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=e2a11eee9d189804182d267a87a4bea2&language=pt-BR&page=1`)
     .then((response) => response.json());
   }
 
   async getDetails(movie_id) {
-    return fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=${process.env.REACT_APP_API_KEY}&language=pt-BR`)
+    return fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=e2a11eee9d189804182d267a87a4bea2&language=pt-BR`)
     .then((response) => response.json());
   }
 
--- src/API/SeriesApiRequest.js
+++ src/API/SeriesApiRequest.js
@@ -1,21 +1,21 @@
 class SeriesRequests {
   async getLatest() {
-    return fetch(`https://api.themoviedb.org/3/tv/latest?api_key=${process.env.REACT_APP_API_KEY}&language=pt-BR`)
+    return fetch(`https://api.themoviedb.org/3/tv/latest?api_key=e2a11eee9d189804182d267a87a4bea2&language=pt-BR`)
     .then((response) => response.json());
   }
 
   async getPopular(page) {
-    return fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${process.env.REACT_APP_API_KEY}&language=pt-BR&page=${page}`)
+    return fetch(`https://api.themoviedb.org/3/tv/popular?api_key=e2a11eee9d189804182d267a87a4bea2&language=pt-BR&page=${page}`)
     .then((response) => response.json());
   }
 
   async getTopRated(page) {
-    return fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=pt-BR&page=${page}`)
+    return fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=e2a11eee9d189804182d267a87a4bea2&language=pt-BR&page=${page}`)
     .then((response) => response.json());
   }
 
   async getDetails(tv_id) {
-    return fetch(`https://api.themoviedb.org/3/tv/${tv_id}?api_key=${process.env.REACT_APP_API_KEY}&language=pt-BR`)
+    return fetch(`https://api.themoviedb.org/3/tv/${tv_id}?api_key=e2a11eee9d189804182d267a87a4bea2&language=pt-BR`)
     .then((response) => response.json());
   }
 
